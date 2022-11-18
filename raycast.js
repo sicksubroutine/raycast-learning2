0847b70bd05dd123109c8b69e89dcfa185123adb
Chaz <790@sicksubroutine.com>
2022-11-18 12:58:11 -0800

    update

--- raycast.js
+++ raycast.js
@@ -1,32 +1,40 @@
 const TILE_SIZE = 32;
-const MAP_NUM_ROWS = 11;
+const MAP_NUM_ROWS = 12;
 const MAP_NUM_COLS = 15;
-
 const WINDOW_WIDTH = MAP_NUM_COLS * TILE_SIZE;
-const WINDOW_HEIGHT= MAP_NUM_ROWS * TILE_SIZE;
-const PI = Math.PI
-const DOUBLE_PI = PI*2
+const WINDOW_HEIGHT = MAP_NUM_ROWS * TILE_SIZE;
+const PI = Math.PI;
+const DOUBLE_PI = PI * 2;
 
 class Map {
     constructor() {
         this.grid = [
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-            [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-            [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+            [1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+            [1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+            [1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1],
+            [1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+            [1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
         ];
     }
+    hasWallAt(x, y) {
+        if (x <0 || x > WINDOW_WIDTH || y < 0 || y > WINDOW_HEIGHT) {
+            return true;
+        }
+        var mapGridIndexX = Math.floor(x / TILE_SIZE);
+        var mapGridIndexY = Math.floor(y / TILE_SIZE);
+        return this.grid[mapGridIndexY][mapGridIndexX] != 0; 
+    }
     render() {
         for (var i = 0; i < MAP_NUM_ROWS; i++) {
             for (var j = 0; j < MAP_NUM_COLS; j++) {
-                var tileX = j * TILE_SIZE; 
+                var tileX = j * TILE_SIZE;
                 var tileY = i * TILE_SIZE;
                 var tileColor = this.grid[i][j] == 1 ? "#222" : "#fff";
                 stroke("#222");
@@ -42,26 +50,32 @@ class Player {
         this.x = WINDOW_WIDTH / 2;
         this.y = WINDOW_HEIGHT / 2;
         this.radius = 3;
-        this.turnDirection = 0; // -1 if left, +1 if right
-        this.walkDirection = 0; // -1 if back, +1 if front
-        this.rotationAngle = PI/2;
+        this.turnDirection = 0; // -1 for left, +1 for right
+        this.walkDirection = 0; // -1 for back, +1 for front
+        this.rotationAngle = PI / 2;
         this.moveSpeed = 2.0;
-        this.rotationSpeed = 2 * (PI / 180);
+        this.rotationSpeed = 3 * (PI / 180);
     }
     update() {
-        this.rotationAngle += this.turnDirection * this.rotationSpeed;
+       this.rotationAngle += this.turnDirection * this.rotationSpeed;
+       var moveStep = this.walkDirection * this.moveSpeed;
+       var newPlayerX = this.x + Math.cos(this.rotationAngle) * moveStep;
+       var newPlayerY = this.y + Math.sin(this.rotationAngle) * moveStep;
+       // only set new player position if it's not colliding with a wall
+       if (!grid.hasWallAt(newPlayerX, newPlayerY)) {
+        this.x = newPlayerX;
+        this.y = newPlayerY;
+       }
     }
     render() {
-        noStroke();
         fill("red");
-        circle(this.x,this.y,this.radius);
+        circle(this.x, this.y, this.radius);
         stroke("red");
         line(
-            this.x,
-            this.y,
-            this.x * Math.cos(this.rotationAngle * 20),
-            this.y * Math.sin(this.rotationAngle * 20)
-        );
+            this.x, 
+            this.y, 
+            this.x + Math.cos(this.rotationAngle) * 50, 
+            this.y + Math.sin(this.rotationAngle) * 50);
     }
 }
 
@@ -69,32 +83,32 @@ var grid = new Map();
 var player = new Player();
 
 function keyPressed() {
-    if (keyCode == UP_ARROW){
+    if (keyCode == UP_ARROW) {
         player.walkDirection = +1;
     } else if (keyCode == DOWN_ARROW) {
         player.walkDirection = -1;
-    } else if (keyCode == LEFT_ARROW) {
-        player.turnDirection = +1;
     } else if (keyCode == RIGHT_ARROW) {
+        player.turnDirection = +1;
+    } else if (keyCode == LEFT_ARROW) {
         player.turnDirection = -1;
     }
+
 }
 
 function keyReleased() {
-    if (keyCode == UP_ARROW){
+    if (keyCode == UP_ARROW) {
         player.walkDirection = 0;
     } else if (keyCode == DOWN_ARROW) {
         player.walkDirection = 0;
-    } else if (keyCode == LEFT_ARROW) {
-        player.turnDirection = 0;
     } else if (keyCode == RIGHT_ARROW) {
+        player.turnDirection = 0;
+    } else if (keyCode == LEFT_ARROW) {
         player.turnDirection = 0;
     }
 }
 
 function setup() {
     createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
-
 }
 
 function update() {
@@ -106,3 +120,5 @@ function draw() {
     grid.render();
     player.render();
 }
+
+
